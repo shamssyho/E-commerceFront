cf1062ce8ca91f49c8cc3e29c29874990500e3ca
unknown <shamss12301230@gmail.com>
2023-08-07 22:48:17 +0200

    add header + footer

---
+++ ecommerce/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1 className="footer-h1">
+        Footer
+      </h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ ecommerce/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1 className="header-h1">AG</h1>
+    </div>
+  )
+}
+
+export default Header
